5bada6d83aa3691863629067eb86f704cea3f59f
Meriton Askaj <meritonaskaj01@gmail.com>
2026-05-18 12:56:11 +0200

    Ajout d'une vague douce pour la section Nos valeurs

--- ecole-du-numerique/src/pages/About/About.jsx
+++ ecole-du-numerique/src/pages/About/About.jsx
@@ -4,6 +4,7 @@ import AboutApproach    from './AboutApproach/AboutApproach'
 import AboutValues      from './AboutValues/AboutValues'
 import AboutTeam        from './AboutTeam/AboutTeam'
 import AboutFinancement from './AboutFinancement/AboutFinancement'
+import Wave             from '../../components/Dividers/Wave'
 
 export default function About() {
   return (
@@ -11,7 +12,9 @@ export default function About() {
       <AboutHero />
       <AboutMission />
       <AboutApproach />
+      <Wave top="#F8F7FC" bottom="#0D0B1A" />
       <AboutValues />
+      <Wave top="#0D0B1A" bottom="#F8F7FC" />
       <AboutTeam />
       <AboutFinancement />
     </>
